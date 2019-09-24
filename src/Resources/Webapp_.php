1bf44e93d09371da1443b66906aa723ada93300d
Craig A Rodway <craig.rodway@gmail.com>
2019-09-24 22:12:30 +0100

    Filename case rename (1/2)

---
+++ src/Resources/Webapp_.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace OnHover\RunCloud\Resources;
+
+
+class WebApp extends Resource
+{
+	public $id;
+	public $serverUserId;
+	public $serverUserUsername;
+	public $name;
+	public $rootPath;
+	public $publicPath;
+	public $phpVersion;
+	public $stack;
+	public $stackMode;
+	public $stackHumanize;
+	public $defaultServer;
+	public $restorationStatus;
+	public $createdAt;
+	public $createdAtHumanize;
+	public $updatedAt;
+	public $updatedAtHumanize;
+
+}
